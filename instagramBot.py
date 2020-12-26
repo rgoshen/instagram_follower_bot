8c4a1273aab35788b3e453fc685f5b808bd5d629
rgoshen <rick.goshen@gmail.com>
2020-12-26 12:22:13 -0700

    created instagramBot follower search method

--- instagramBot.py
+++ instagramBot.py
@@ -3,8 +3,8 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 SIMILAR_ACCOUNT = "chefsteps"
-USERNAME = "rgoshen72"
-PASSWORD = "NitL10n$#1"
+USERNAME = "username"
+PASSWORD = "password"
 
 
 class InstaFollower:
@@ -35,7 +35,26 @@ class InstaFollower:
         password.send_keys(Keys.ENTER)
 
     def find_followers(self):
-        pass
+        """
+        Method to search for followers on instagram.
+        """
+        time.sleep(5)
+        self.driver.get(f"https://www.instagram.com/{SIMILAR_ACCOUNT}")
+
+        time.sleep(2)
+        followers = self.driver.find_element_by_xpath(
+            '//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a')
+        followers.click()
+
+        time.sleep(2)
+        modal = self.driver.find_element_by_xpath('/html/body/div[4]/div/div/div[2]')
+        for i in range(10):
+            # In this case we're executing some Javascript, that's what the execute_script() method does.
+            # The method can accept the script as well as a HTML element.
+            # The modal in this case, becomes the arguments[0] in the script.
+            # Then we're using Javascript to say: "scroll the top of the modal (popup) element by the height of the modal (popup)"
+            self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", modal)
+            time.sleep(2)
 
     def follow(self):
         pass
