5b532ad86d358e2bf911a0322bdf81dfc4d4b5f2
rgoshen <rick.goshen@gmail.com>
2020-12-26 11:54:21 -0700

    created instagramBot class

--- instagramBot.py
+++ instagramBot.py
@@ -5,3 +5,24 @@ import time
 SIMILAR_ACCOUNT = "chefsteps"
 USERNAME = "rgoshen72"
 PASSWORD = "NitL10n$#1"
+
+
+class InstaFollower:
+    """
+    Class to create an instagram follower bot.
+    """
+
+    def __init__(self, path):
+        """
+        Instagram follower bot constructor.
+        """
+        self.driver = webdriver.Chrome(executable_path=path)
+
+    def login(self):
+        pass
+
+    def find_followers(self):
+        pass
+
+    def follow(self):
+        pass
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-
+from instagramBot import InstaFollower
 
 CHROME_DRIVER_PATH = "/home/rgoshen/Development/chromedriver"
+
+bot = InstaFollower(CHROME_DRIVER_PATH)
+bot.login()
+bot.find_followers()
+bot.follow()
